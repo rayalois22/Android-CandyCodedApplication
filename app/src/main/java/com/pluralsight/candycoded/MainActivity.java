e10ee10bd12f99f7287ed6a1f9880dea3ca9dde3
Alois Odhiambo <rayalois22@gmail.com>
2020-10-14 06:42:29 +0300

    Task 1 - Show Store Information Activity - Done

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -87,13 +87,10 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.info) {
+            // Shows Store Information Activity using an explicit intent
             startActivity(new Intent(this, InfoActivity.class));
             return true;
         }
